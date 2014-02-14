10be916006e2e638460e97cb0e9aa7b9d7679470
ms0da <ms0da@ms0da.com>
2014-02-14 10:09:17 -0500

    chore(locked_queue): add locked_queue and test

---
+++ src/locked_queue.hpp
@@ -0,0 +1,55 @@
+
+#ifndef LMQ_LOCKED_QUEUE_HPP
+#define LMQ_LOCKED_QUEUE_HPP
+
+#include <list>
+#include <mutex>
+
+namespace lmq {
+
+    template<typename T>
+    struct locked_queue {
+        typedef T elem_type;        
+    private:
+        typedef std::mutex mutex_type;
+        typedef std::lock_guard<mutex_type> lock_type;
+        typedef std::list<elem_type> list_type;
+    public:
+        typedef typename list_type::size_type size_type;
+
+        bool empty() const {
+            return _list.empty();
+        }
+
+        void push_back(const elem_type& e) {
+            lock_type lock(_mutex);
+            _list.emplace_back(e);
+        }
+
+        const elem_type& front() {
+            lock_type lock(_mutex);
+            return _list.front();
+        }
+
+        void pop_front() {
+            lock_type lock(_mutex);
+            _list.pop_front();
+        }
+
+        size_type size() const {
+            return _list.size();
+        }
+        
+        void clear() {
+            lock_type lock(_mutex);
+            _list.clear();
+        }
+
+    private:
+        list_type _list;
+        mutex_type _mutex;
+    };
+
+}
+
+#endif
---
+++ test/locked_queue_test.cpp
@@ -0,0 +1,87 @@
+
+#include <random>
+
+#include "../src/locked_queue.hpp"
+#include "../ext/catch/catch.hpp"
+
+void random_populate_queue(lmq::locked_queue<int> &q, const int min, const int max) {
+    std::random_device rd;
+    std::mt19937 generator(rd());
+    std::uniform_int_distribution<> dis(min, max);
+
+    auto count = dis(generator);
+    for(int i = 0; i < count; ++i) {
+        auto elem = dis(generator);
+        q.push_back(elem);
+    }
+}
+
+
+SCENARIO("lockeq queue") {
+    using lmq::locked_queue;
+    typedef locked_queue<int> queue_type;
+    queue_type q;
+    queue_type::elem_type element_added = 1;
+    const queue_type::size_type min_count = 2;
+    const queue_type::size_type max_count = 100;
+
+    GIVEN("an empty queue") {
+        REQUIRE(true == q.empty());
+        REQUIRE(0 == q.size());
+
+        WHEN("an element is added") {
+            q.push_back(element_added);
+
+            THEN("the size is larger") {
+                REQUIRE(true != q.empty());
+                REQUIRE(q.size() > 0);
+            }
+        }
+    }
+    GIVEN("a queue with 1 element") {
+        q.push_back(element_added);
+        REQUIRE(false == q.empty());
+        REQUIRE(1 == q.size());
+
+        WHEN("an element is removed") {
+            auto element_removed = q.front();
+            q.pop_front();
+
+            THEN("the size is zero") {
+                REQUIRE(true == q.empty());
+                REQUIRE(0 == q.size());
+                REQUIRE(element_added == element_removed);
+            }
+        }
+    }
+    GIVEN("a queue with more than 2 elements and less than 100 elements") {
+        random_populate_queue(q, min_count, max_count);
+        const auto queue_size = q.size();
+
+
+        WHEN("an element is removed") {
+            q.pop_front();
+
+            THEN("the size is smaller") {
+                const auto new_size = queue_size - 1;
+                REQUIRE(new_size == q.size());
+            }
+        }
+        WHEN("an element is added") {
+            q.push_back(0);
+
+            THEN("the size is larger") {
+                const auto new_size = queue_size + 1;
+                REQUIRE(new_size == q.size());
+            }
+        }
+        WHEN("the queue is cleared") {
+            q.clear();
+
+            THEN("the size is zero") {
+                REQUIRE(true == q.empty());
+                REQUIRE(0 == q.size());
+            }
+        }
+    }
+}
